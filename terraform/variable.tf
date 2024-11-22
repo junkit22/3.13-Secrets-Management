cd057d95b620f05d77da8c54ad1b2da54ce2e6f0
junkit22 <junkit22@gmail.com>
2024-11-22 11:23:55 +0800

    update

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,26 +1,13 @@
 
+
 variable "lambda_function_name" {
   description = "Name of lambda function"
   type        = string
-  default     = "junjie-lambda-tf"
+  default     = "junjie-lambda"
 }
 
 variable "lambda_file_name" {
   description = "Name of lambda file to be zipped"
   type        = string
-  default     = "junjie-lambda-tf"
-}
-
-# IAM lambda role
-variable "iam_lambda_role" {
-  description = "IAM role Name for lambda"
-  type        = string
-  default     = "ce7-junjie-iam-lambda-role"
-}
-
-# IAM cloudwatch policy name
-variable "iam_cloudwatch_policy" {
-  description = "IAM cloudwatch policy name for lambda"
-  type        = string
-  default     = "ce7-junjie-iam-cloudwatch-policy"
+  default     = "junjie-lambda"
 }
